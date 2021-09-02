e2f4cfa9dd97853bd665d8e93df6ef77eb473087
MauricioGH92 <merchan.ber.maur@gmail.com>
2021-09-02 09:16:44 -0500

    add context for cv

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -12,11 +13,41 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        Here is My-CV
+      <div className="About">
+        <div className="card">
+          <div className="card_details">
+            <div className="card_photo center circle">
+              <img src="IMG_20210828_094149_2.jpg" alt="Michael Merchan"/>
+              {/* <svg viewBox="0 0 100 100"  style="enable-background:new -580 439 577.9 194;"
+                xml:space="preserve">
+                <circle cx="50" cy="50" r="50" />
+              </svg> */}
+            </div>
+            <p className="card_title">Hi, My name is</p>
+            <p className="card_value">Michael Merchan</p>
+          </div>
+          <div className="card_userdata">
+            <ul>
+              <li>merchan.ber.mau@gmail.com</li>
+              <li>Ecuador</li>
+            </ul>
+          </div>
+          <div className="card_social">
+            <a href="https://twitter.com/gndx">
+              <img src="${twitter}" />
+            </a>
+            <a href="https://github.com/gndx">
+              <img src="${github}" />
+            </a>
+            <a href="https://instagram.com/gndx">
+              <img src="${instagram}" />
+            </a>
+          </div>
+        </div>
+      </div>
       </main>
 
       <footer className={styles.footer}>
-        
           Powered by{' '}
           MauricioGH92
       </footer>
